cc94a862897476ae9258a4aa166a969784666f3e
Rafael Telles <rafast.telles@gmail.com>
2022-06-08 20:42:05 -0300

    Commit do projeto completo

---
+++ ImobiliariaVirtual/Controllers/ImovelController.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using ImobiliariaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImobiliariaVirtual.Controllers
+{
+    public class ImovelController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            IEnumerable<Imovel> imoveis = null;
+            using (var imoveisAPI = new HttpClient())
+            {
+                imoveisAPI.BaseAddress = new Uri("https://62a0e8127b9345bcbe42018c.mockapi.io/api/v1/");
+
+                var resposta = imoveisAPI.GetAsync("imoveis");
+                resposta.Wait();
+
+                var resultado = resposta.Result;
+                if (resultado.IsSuccessStatusCode)
+                {
+                    var conteudoResposta = resultado.Content.ReadAsStringAsync();
+                    conteudoResposta.Wait();
+                    imoveis = JsonSerializer.Deserialize<IList<Imovel>>(conteudoResposta.Result);
+                }
+            }
+            return View(imoveis);
+        }
+
+        //GET Direciona o usuário para o formulário de criação de imóveis
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST Recebe um imóvel da CreateView de imóveis e faz uma chamada para a API criar o novo imóvel
+        [HttpPost]
+        public IActionResult Create(Imovel obj)
+        {
+            using (var imoveisAPI = new HttpClient())
+            {
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "https://62a0e8127b9345bcbe42018c.mockapi.io/api/v1/imoveis");
+                string json = JsonSerializer.Serialize(obj);
+                request.Content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = imoveisAPI.SendAsync(request);
+                response.Wait();
+
+                if (response.Result.IsSuccessStatusCode)
+                {
+                    TempData["success"] = "Imóvel criado com sucesso!";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["error"] = "O servidor está fora do ar. Tente novamente mais tarde.";
+                }
+            }
+            return NotFound();
+        }
+
+        //GET Direciona o usuário para a EditView e carrega os dados do imóvel a ser alterado
+        public IActionResult Edit(int? id)
+        {
+            if (id is null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Imovel imovel = null;
+            using (var imoveisAPI = new HttpClient())
+            {
+                imoveisAPI.BaseAddress = new Uri("https://62a0e8127b9345bcbe42018c.mockapi.io/api/v1/");
+                var response = imoveisAPI.GetAsync("imoveis/" + id.ToString());
+                response.Wait();
+
+                if (response.Result.IsSuccessStatusCode)
+                {
+                    var content = response.Result.Content.ReadAsStringAsync();
+                    content.Wait();
+                    imovel = JsonSerializer.Deserialize<Imovel>(content.Result);
+                }
+            }
+
+            return View(imovel);
+        }
+
+        //POST Recebe um imóvel da EditView de imóveis e faz uma chamada para a API atualizar os dados imóvel
+        [HttpPost]
+        public IActionResult Edit(Imovel obj)
+        {
+            using (var imoveisAPI = new HttpClient())
+            {
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Put, "https://62a0e8127b9345bcbe42018c.mockapi.io/api/v1/imoveis/" + obj.Id);
+                string json = JsonSerializer.Serialize(obj);
+                request.Content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = imoveisAPI.SendAsync(request);
+                response.Wait();
+
+                if (response.Result.IsSuccessStatusCode)
+                {
+                    TempData["success"] = "Imóvel atualizado com sucesso!";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["error"] = "O servidor está fora do ar. Tente novamente mais tarde.";
+                }
+            }
+            return NotFound();
+        }
+
+        //GET Direciona o usuário para a DeleteView e carrega os dados do imóvel a ser deletado
+        public IActionResult Delete(int? id)
+        {
+            if (id is null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Imovel imovel = null;
+            using (var imoveisAPI = new HttpClient())
+            {
+                imoveisAPI.BaseAddress = new Uri("https://62a0e8127b9345bcbe42018c.mockapi.io/api/v1/");
+                //HTTP GET
+                var response = imoveisAPI.GetAsync("imoveis/" + id.ToString());
+                response.Wait();
+
+                if (response.Result.IsSuccessStatusCode)
+                {
+                    var content = response.Result.Content.ReadAsStringAsync();
+                    content.Wait();
+                    imovel = JsonSerializer.Deserialize<Imovel>(content.Result);
+                }
+                else
+                {
+                    TempData["error"] = "O servidor está fora do ar. Tente novamente mais tarde.";
+                }
+            }
+
+            return View(imovel);
+        }
+
+        //POST Recebe um imóvel da DeleteView de imóveis e faz uma chamada para a API deletar o imóvel
+        [HttpPost]
+        public IActionResult Delete(Imovel obj)
+        {
+            using (var imoveisAPI = new HttpClient())
+            {
+                imoveisAPI.BaseAddress = new Uri("https://62a0e8127b9345bcbe42018c.mockapi.io/api/v1/");
+
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Delete, "https://62a0e8127b9345bcbe42018c.mockapi.io/api/v1/imoveis/" + obj.Id);
+                string json = JsonSerializer.Serialize<Imovel>(obj);
+                request.Content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = imoveisAPI.SendAsync(request);
+                response.Wait();
+
+                var result = response.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    TempData["success"] = "Imóvel deletado com sucesso!";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["error"] = "O servidor está fora do ar. Tente novamente mais tarde.";
+                }
+            }
+            return NotFound();
+        }
+
+        //GET Direciona o usuário para a DetailView e carrega os dados do imóvel a ser detalhado
+        public IActionResult Detail(int? id)
+        {
+            if (id is null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Imovel imovel = null;
+            using (var imoveisAPI = new HttpClient())
+            {
+                imoveisAPI.BaseAddress = new Uri("https://62a0e8127b9345bcbe42018c.mockapi.io/api/v1/");
+                var response = imoveisAPI.GetAsync("imoveis/" + id.ToString());
+                response.Wait();
+
+                if (response.Result.IsSuccessStatusCode)
+                {
+                    var content = response.Result.Content.ReadAsStringAsync();
+                    content.Wait();
+                    imovel = JsonSerializer.Deserialize<Imovel>(content.Result);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+
+            return View(imovel);
+        }
+    }
+}
---
+++ ImobiliariaVirtual/Models/Imovel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ImobiliariaVirtual.Models
+{
+    public class Imovel
+    {
+        public string Id { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "O tipo não deve ser maior que 50 caracteres")]
+        public string Tipo { get; set; }
+        [DisplayName("Valor de Venda")]
+        [Range(1, 100000000, ErrorMessage = "O valor de venda deve ser entre 1 e 100M")]
+        public float ValorVenda { get; set; }
+        [DisplayName("Valor de Locação")]
+        [Range(1, 500000, ErrorMessage = "O valor de locação deve ser entre 1 e 500k")]
+        public float ValorLocacao { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "O endereco não deve ser maior que 50 caracteres")]
+        [DisplayName("Endereço")]
+        public string Endereco { get; set; }
+        [Required]
+        [DisplayName("Número")]
+        public int Numero { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "O complemento não deve ser maior que 100 caracteres")]
+        public string Complemento { get; set; }
+        [Required]
+        [StringLength(30, ErrorMessage = "O bairro não deve ser maior que 50 caracteres")]
+        public string Bairro { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "A cidade não deve ser maior que 50 caracteres")]
+        public string Cidade { get; set; }
+        [Required]
+        public string Estado { get; set; }
+        [Required]
+        [StringLength(8, ErrorMessage = "O CEP não deve ser maior que 8 caracteres (somente os dígitos)")]
+        public string CEP { get; set; }
+    }
+}
